1bd08073d94c7b14579c2f04088671d75ac2a6c7
mohammadghodoosi <70627259+mohammadghodoosi@users.noreply.github.com>
2024-07-05 15:53:27 +0100

    how to handle click event

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 function Home () {
+    const clickMe = (a)=>{
+        alert('hi'+ a)
+    }
     return ( 
         <div className="home">
-                home page
+                <button onClick={(e)=>clickMe(e.target)}>click me</button>
         </div>
      );
 }
