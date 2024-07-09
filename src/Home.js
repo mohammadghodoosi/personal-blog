1ed6241a42c96c3e880a73bf470221c4f8fa1d01
mohammadghodoosi <70627259+mohammadghodoosi@users.noreply.github.com>
2024-07-09 21:51:44 +0100

    making a custom hook and adding the fetch code to it

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 function Home() {
 
-  const [posts, setPosts] = useState(null);
-  const [loading,setLoading]=useState(true);
-  const [ermessage,setErmessage]=useState(null)
+  const{posts,loading,ermessage}=useFetch("http://localhost:8000/blogs")
+  // const handleDelete = (a) => {
+  //   let newBlogs = posts.filter((b) => b.id !== a);
+  //   setPosts(newBlogs);
+  // };
 
-
-
-  const handleDelete = (a) => {
-    let newBlogs = posts.filter((b) => b.id !== a);
-    setPosts(newBlogs);
-  };
-
-
-  useEffect(() => {
-    setTimeout(()=>{
-      fetch("http://localhost:8000/blogs").then(
-        data=> {return data.json()}
-       ).then(data=>{ setPosts(data); setLoading(false)}).catch(
-        err=>{setErmessage(err.message);setLoading(false)}
-       )   
-    },1000)
-  }, []);
+ 
 
   return (
     <div className="home">
       {ermessage&&<div>{ermessage}</div>}
       {loading&&<h1>loading...</h1>}
-      {posts&&<BlogList p={posts} handleDelete={handleDelete} />}
+      {posts&&<BlogList p={posts}  />}
     </div>
   );
 }
